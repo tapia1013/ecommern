3e99d6ef8639ac86fd19623781273afa808df850
tapia1013 <manbearpig1013@gmail.com>
2022-04-29 20:45:03 -0700

    installed react bootstrap and fontawesome, created header and fotoer and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import { Container } from 'react-bootstrap';
 
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome lol</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
