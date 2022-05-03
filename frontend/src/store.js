058ea63d86505aa41ddb5dc06f3bee92f3074c8e
tapia1013 <manbearpig1013@gmail.com>
2022-05-02 20:02:58 -0700

    created product Action Reducer Constants files and logic in them

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,8 +6,12 @@ import {
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({
+import { productListReducer } from './reducers/productReducers';
 
+
+
+const reducer = combineReducers({
+  productList: productListReducer
 })
 
 const initialState = {};
