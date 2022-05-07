0ad7ccb504e602d83f075b395fcb5cf8313901be
tapia1013 <manbearpig1013@gmail.com>
2022-05-06 17:46:46 -0700

    updated profile endpoint backend controller and route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -71,7 +71,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 
-// @desc     Auth user profile
+// @desc     Get user profile
 // @route    GET /api/users/profile
 // @access   Private (we need token)
 const getUserProfile = asyncHandler(async (req, res) => {
@@ -96,8 +96,42 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 
 
+// @desc     Update user profile
+// @route    PUT /api/users/profile
+// @access   Private (we need token)
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+
+  } else {
+    // 404 = not found
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+
+
 export {
   authUser,
   getUserProfile,
-  registerUser
+  registerUser,
+  updateUserProfile
 }
